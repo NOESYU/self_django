c7e0cd3be347e7430394c8692ed2668b7b292b2b
seonyu <ssunuu114@gmail.com>
2021-07-12 11:16:00 +0900

    210712 self commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def hihi(request):
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == 'POST':
+        temp = request.POST.get('input_text')
+        return render(request, 'accountapp/hello_world.html',
+                      context={'text': temp})
+    elif request.method == 'GET':
+        return render(request, 'accountapp/hello_world.html',
+                      context={'text':'Get Method'})
