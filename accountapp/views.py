aaf001603663690fdac7848f64091d1453e528c0
seonyu <ssunuu114@gmail.com>
2021-07-12 11:34:21 +0900

    210712 self commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,12 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import HelloWorld
+
 
 def hihi(request):
     if request.method == 'POST':
         temp = request.POST.get('input_text')
+
+        new_hello_world = HelloWorld() #model.py 에 있는 객체 새롭게 생성
+        new_hello_world.text = temp
+        new_hello_world.save() #디비에 저장
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': temp})
+                      context={'new_hello_world' : new_hello_world})
+
     elif request.method == 'GET':
         return render(request, 'accountapp/hello_world.html',
                       context={'text':'Get Method'})
