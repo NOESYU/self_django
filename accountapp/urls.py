126f39a03c5f738f0956a2157b214b7884bc94c9
seonyu <ssunuu114@gmail.com>
2021-07-13 17:08:48 +0900

    210713 self commit 1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from accountapp.views import hihi
+from accountapp.views import hihi, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [
     path('hihi/', hihi, name='hi'),
+    path('create/', AccountCreateView.as_view(), name='create')
 ]
