570878e49b44d0900298ff43c4a5e02487775be1
seonyu <ssunuu114@gmail.com>
2021-07-22 11:57:25 +0900

    210722 self commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,16 +1,19 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.forms import AccountCreationForms
 from accountapp.models import HelloWorld
 
 
+@login_required(login_url=reverse_lazy('accountapp:login'))
 def hihi(request):
     if request.method == 'POST':
         temp = request.POST.get('input_text')
@@ -25,7 +28,7 @@ def hihi(request):
     elif request.method == 'GET':
         hello_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html',
-                      context={'hello_list': hello_list})
+                    context={'hello_list': hello_list})
 
 class AccountCreateView(CreateView):
     model = User
@@ -38,6 +41,8 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
+@method_decorator(login_required(login_url=reverse_lazy('accountapp:login')), 'get')
+@method_decorator(login_required(login_url=reverse_lazy('accountapp:login')), 'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForms
@@ -45,6 +50,8 @@ class AccountUpdateView(UpdateView):
     context_object_name = 'target_user'
     template_name = 'accountapp/update.html'
 
+@method_decorator(login_required(login_url=reverse_lazy('accountapp:login')), 'get')
+@method_decorator(login_required(login_url=reverse_lazy('accountapp:login')), 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
