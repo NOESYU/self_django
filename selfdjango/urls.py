e6478408d39cb9205ad16d4ff239eba48c4cfc9d
seonyu <ssunuu114@gmail.com>
2021-08-02 11:14:11 +0900

    210802 self commit 1 추가,,,

--- selfdjango/urls.py
+++ selfdjango/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('accountapp.urls')),
-    path('profile/', include('profileapp.urls'))
+    path('profile/', include('profileapp.urls')),
+    path('article/', include('articleapp.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
