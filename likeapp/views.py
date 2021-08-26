ec3b31d062229f1fb827802f87022b98fc474488
seonyu <ssunuu114@gmail.com>
2021-08-26 11:33:12 +0900

    210826 self commit 1

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,4 +1,6 @@
 from django.contrib import messages
+from django.core.exceptions import ValidationError
+from django.db import transaction
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -10,6 +12,19 @@ from articleapp.models import Article
 from likeapp.models import LikeRecord
 
 
+@transaction.atomic
+def db_transaction(user, article):
+    article.like += 1
+    article.save()
+
+    like_record = LikeRecord.objects.filter(user=user,
+                                            article=article)
+    if like_record.exists():
+        raise ValidationError('like already exists.')
+    else:
+        LikeRecord(user=user, article=article).save()
+
+
 class LikeArticleView(RedirectView):
     def get(self, request, *args, **kwargs):
         user = request.user
@@ -18,16 +33,18 @@ class LikeArticleView(RedirectView):
         like_record = LikeRecord.objects.filter(user=user,
                                                 article=article)
 
-        if like_record.exists():
+        try:
+            # 좋아요 반영
+            db_transaction(user, article)
+            messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다.')
+
+
+        except ValidationError:
+            # 좋아요 반영 안되게
             messages.add_message(request, messages.ERROR, '좋아요는 한번만 가능합니다.')
             return HttpResponseRedirect(reverse('articleapp:detail',
-                                                kwargs={'pk':kwargs['article_pk']}))
+                                                kwargs={'pk': kwargs['article_pk']}))
 
-        else:
-            LikeRecord(user=user, article=article).save()
-            article.like += 1
-            article.save()
-            messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다.')
         return super().get(request, *args, **kwargs)
 
     def get_redirect_url(self, *args, **kwargs):
